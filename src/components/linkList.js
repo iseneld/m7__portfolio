b5febf9b5435d69e0c904a4eccda28dfddf29f42
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-10 22:27:36 +0100

    Sticky banner and extended link list.

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -1,15 +1,21 @@
 const listItems = [
-  {
-    title: "Code Wars",
-    url: "https://www.codewars.com/users/iseneld",
-    img: "codewars.png",
-    class: "corewars",
-  },
+  // {
+  //   title: "Code Wars",
+  //   url: "https://www.codewars.com/users/iseneld",
+  //   img: "codewars.png",
+  //   class: "corewars",
+  // },
   {
     title: "Github",
     url: "https://github.com/iseneld",
     img: "github.png",
     class: "github",
+  },
+  {
+    title: "Instagram",
+    url: "https://www.instagram.com/micheliseneld/",
+    img: "photography.jpg",
+    class: "photography",
   },
   {
     title: "Linkedin",
@@ -29,24 +35,18 @@ const listItems = [
     img: "slink.jpg",
     class: "slink",
   },
-  // {
-  //   title: "Aether Mechanics",
-  //   url: "https://aethermechanics.bandcamp.com/",
-  //   img: "aethermechanics.jpg",
-  //   class: "aether-mechanics",
-  // },
-  // {
-  //   title: "Ntogn",
-  //   url: "https://soundcloud.com/ntogn",
-  //   img: "ntogn.jpg",
-  //   class: "ntogn",
-  // },
-  // {
-  //   title: "Photography",
-  //   url: "https://www.instagram.com/micheliseneld/",
-  //   img: "photography.jpg",
-  //   class: "photography",
-  // },
+  {
+    title: "Aether Mechanics",
+    url: "https://aethermechanics.bandcamp.com/",
+    img: "aethermechanics.jpg",
+    class: "aether-mechanics",
+  },
+  {
+    title: "Ntogn",
+    url: "https://soundcloud.com/ntogn",
+    img: "ntogn.jpg",
+    class: "ntogn",
+  },
   // {
   //   title: "Aedi Records",
   //   url: "https://aedirecords.bandcamp.com/",
