b98bf510277a52d2ae6d7faf37ffec46fab28ee0
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-11 09:39:29 +0100

    Updated CSS and added second section to main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,31 +12,39 @@ function Main(props) {
     <>
       <Header />
       <main>
-        <section className="landing__banner">
-          <h1>This is my first school project using React.js</h1>
-        </section>
-        <section className="landing-list">
-          <div className="michel"></div>
-          <section>
-            <h1>🔗</h1>
-            <ul>
-              {linkList.map((x) => (
-                <li
-                  className={`landing-list`}
-                  key={x.id} // KEY PROP ADDED HERE!
-                  style={{
-                    backgroundImage: `url(./images/${x.img})`,
-                  }}
-                >
-                  <h2>
-                    <a href={x.url} target="_blank" rel="noreferrer">
-                      {x.title}{" "}
-                    </a>
-                  </h2>
-                </li>
-              ))}
-            </ul>
+        <section className="landing__top">
+          <section className="landing__banner">
+            <h1>This is my first school project using React.js</h1>
           </section>
+          <section className="landing-list">
+            <div className="michel"></div>
+            <section>
+              <h1>🔗</h1>
+              <ul>
+                {linkList.map((x) => (
+                  <li
+                    className={`landing-list`}
+                    key={x.id} // KEY PROP ADDED HERE!
+                    style={{
+                      backgroundImage: `url(./images/${x.img})`,
+                    }}
+                  >
+                    <h2>
+                      <a href={x.url} target="_blank" rel="noreferrer">
+                        {x.title}{" "}
+                      </a>
+                    </h2>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </section>
+        </section>
+        <section className="landing__second">
+          <div>
+            <h2>🚧 Under Construction 🚧</h2>
+            <p>Building publicly to get some fire under my butt</p>
+          </div>
         </section>
       </main>
       <Footer />
