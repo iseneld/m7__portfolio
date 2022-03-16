d08955206259aaa176d6531b21aa450407ba5836
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-16 15:25:37 +0100

    Now we're in business!

--- src/components/Streaming.js
+++ src/components/Streaming.js
@@ -32,25 +32,23 @@ function Streaming() {
         <section className="landing__top">
           <section className="streaming__banner">
             <aside></aside>
-            <section className="streaming__list">
-              <ul className="resultList">
-                {tracks &&
-                  tracks.map((x) => {
-                    return (
-                      <li key={x._id + "x"}>
-                        <button className="button-play">
-                          {x.artist} - {x.title}
-                          <audio controls>
-                            <source src={x.url} type="audio/mpeg" />
-                            Your browser does not support the audio element.
-                          </audio>
-                        </button>
-                        <button className="button-do button-del">🗑️</button>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </section>
+            <ul className="streaming__results">
+              {tracks &&
+                tracks.map((x) => {
+                  return (
+                    <li key={x._id + "x"}>
+                      <button className="button-play">
+                        {x.artist} - {x.title}
+                        <audio controls>
+                          <source src={x.url} type="audio/mpeg" />
+                          Your browser does not support the audio element.
+                        </audio>
+                      </button>
+                      <button className="button-do button-del">🗑️</button>
+                    </li>
+                  );
+                })}
+            </ul>
           </section>
           <section className="streaming__main">
             <div className="toolbar">
@@ -67,25 +65,23 @@ function Streaming() {
                 </li>
               </ul>
             </div>
-            <div className="streaming__list">
-              <ul className="resultList">
-                {tracks &&
-                  tracks.map((x) => {
-                    return (
-                      <li key={x._id}>
-                        <button className="button-play">
-                          {x.artist} - {x.title}
-                          <audio controls>
-                            <source src={x.url} type="audio/mpeg" />
-                            Your browser does not support the audio element.
-                          </audio>
-                        </button>
-                        <button className="button-do button-fav">❤️</button>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
+            <ul className="streaming__results">
+              {tracks &&
+                tracks.map((x) => {
+                  return (
+                    <li key={x._id}>
+                      <button className="button-play">
+                        {x.artist} - {x.title}
+                        <audio controls>
+                          <source src={x.url} type="audio/mpeg" />
+                          Your browser does not support the audio element.
+                        </audio>
+                      </button>
+                      <button className="button-do button-fav">❤️</button>
+                    </li>
+                  );
+                })}
+            </ul>
           </section>
         </section>
       </main>
