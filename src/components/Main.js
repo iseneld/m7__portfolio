b036610babc27ffba6656c2fbf0b17c97ec49f0c
iseneld <84731866+iseneld@users.noreply.github.com>
2022-03-23 16:28:32 +0100

    Updated front page information.

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ function Main(props) {
       <main>
         <section className="landing__top">
           <section className="landing__banner">
-            <h1>This is my first school project using React.js</h1>
+            <h1>I built this school project using Sass and the MERN stack.</h1>
           </section>
           <section className="landing-list">
             <div className="michel"></div>
