2f4898208b04a2f647d707ea45672857a7720f8e
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-11 00:21:22 +0200

    La till fotografi.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         target="_blank"
         rel="noreferrer"
       >
-        Code and design by Michel Iseneld
+        Code, design and photography by Michel Iseneld
       </a>
     </footer>
   );
