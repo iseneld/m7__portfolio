fdaf88564cc2d4fa5d047e22605c9b92ddd98230
iseneld <84731866+iseneld@users.noreply.github.com>
2022-05-01 11:41:48 +0200

    Styling and variables set up. Bar barely working.

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react';
 import './progress-bar.css';
 
 export const ProgressBar = () => {
+  const handleScroll = () => document.documentElement.style.setProperty('--progress-width', `${window.pageYOffset/80}%`);;
+  let body = document.body;
+  let html = document.documentElement;
+  let height = Math.max( body.scrollHeight, body.offsetHeight, 
+      html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    const width = getComputedStyle(document.documentElement).getPropertyValue('--progress-width');
+    console.log(width);
+    console.log(height);
+    console.log(window);
+    // window.scrollbars.visible = false;
+  }, [height])
+
   return (
     <div className="progress-bar">
       <div className="progress-bar__progress"></div>
