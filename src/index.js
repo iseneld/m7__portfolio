09b7e2f3575c23f1bb3ba377afbf9125bcb2c378
iseneld <84731866+iseneld@users.noreply.github.com>
2022-05-11 12:11:00 +0200

    Page now scrolls to top when rerouted.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
+// HELPERS
+import ScrollToTop from "./helpers/scrollToTop";
+
 // MAIN CSS
 import "./styles/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
