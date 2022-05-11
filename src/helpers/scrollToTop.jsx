83cd25ab61dda05c72fe32e2ad654d4f0b524cbc
iseneld <84731866+iseneld@users.noreply.github.com>
2022-05-11 12:15:03 +0200

    Added link to stack overflow solution.

--- src/helpers/scrollToTop.jsx
+++ src/helpers/scrollToTop.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+// 2022-05-11: Copied from https://stackoverflow.com/questions/58598637/why-react-new-page-render-from-the-bottom-of-the-screen/58598723#58598723
+
 const ScrollToTop = ({ children }) => {
   const { pathname } = useLocation();
 
