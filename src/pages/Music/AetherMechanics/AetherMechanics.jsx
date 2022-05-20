701f39fbac03e7d69fca02d7cdbf481bfa3c5d26
iseneld <84731866+iseneld@users.noreply.github.com>
2022-05-20 10:46:05 +0200

    Added images.

--- src/pages/Music/AetherMechanics/AetherMechanics.jsx
+++ src/pages/Music/AetherMechanics/AetherMechanics.jsx
@@ -7,7 +7,12 @@ export default function AetherMechanics(props) {
     <>
       <ProgressBar />
       <main>
-        <Section page="music" image="studio-1" text="Bajs." height={59} />
+        <Section
+          page="music"
+          image="/aethermechanics/aether1"
+          text="Aether Mechanics"
+          height={59}
+        />
         <section className="link-list">
           <ul style={{ borderTop: "1rem solid var(--color-dark)" }}>
             <li>
@@ -16,36 +21,36 @@ export default function AetherMechanics(props) {
                 target="_blank"
                 rel="noreferrer"
               >
-                <h2>Aether Mechanics</h2>
+                <h2>Bandcamp</h2>
                 <span>{`>`}</span>
               </a>
             </li>
             <li>
               <a
-                href="https://tomerecords.bandcamp.com/"
+                href="https://soundcloud.com/aethermechanics"
                 target="_blank"
                 rel="noreferrer"
               >
-                <h2>Ntogn</h2>
+                <h2>Soundcloud</h2>
                 <span>{`>`}</span>
               </a>
             </li>
             <li>
               <a
-                href="https://hypnus.bandcamp.com/"
+                href="https://music.youtube.com/channel/UCGvPehEP20Gq9VV7R_JXYtQ?feature=share"
                 target="_blank"
                 rel="noreferrer"
               >
-                <h2>Hypnus Records</h2>
+                <h2>Youtube Music</h2>
                 <span>{`>`}</span>
               </a>
             </li>
           </ul>
         </section>
-        <Section page="music" image="modular-1" />
+        <Section page="music" image="aethermechanics/aether3" />
         <section>
           <div className="info">
-            <h2>Under construction..</h2>
+            <h2>Under construction 🚧</h2>
           </div>
         </section>
       </main>
