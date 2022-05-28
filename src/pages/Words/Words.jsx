2126f2f9b4d559857cb997e54f40a00e3c259951
iseneld <84731866+iseneld@users.noreply.github.com>
2022-05-28 19:07:56 +0200

    Added a tonne of words.

--- src/pages/Words/Words.jsx
+++ src/pages/Words/Words.jsx
@@ -39,11 +39,16 @@ export default function Words() {
           </section>
         </section>
         <Banner text="You're not experiencing. You are the experience." />
+        <Banner text="If you want to understand your mind, sit down and observe it." />
         <Banner text="Creativity brings order to chaos." />
+        <Banner text="The surest way to predict the future is to create it." />
         <Banner text="Innovation happens at the speed of trust." />
         <Banner text="Iteration outperforms design." />
-        <Banner text="The surest way to predict the future is to create it." />
         <Banner text="Consistency compounds." />
+        {/* <Banner text="Make progress, not perfect." /> */}
+        <Banner text="Progress is propelled by problems. " />
+        <Banner text="Being wrong means you've grown." />
+        <Banner text="All mistakes can be good mistakes, if you learn." />
         <Banner text="Take risks." />
         <Banner
           text="Fail fast."
@@ -52,22 +57,42 @@ export default function Words() {
           }}
         />
         <Section image="4" height={79} />
+        <Banner
+          text="Read slowly."
+          stylez={{
+            backgroundColor: "var(--color-blood)",
+            color: "var(--color-white)",
+          }}
+        />
+        <Banner text="Identify with your ignorance." />
+        <Banner text="Determinism cures hatred." />
         <Banner text="There is no free will." />
         <Banner text="We do not control, we react." />
-        <Banner text="Life is inherently meaningless." />
-        <Banner text="We create meaning through sacrifices." />
+        <Banner text="Pain and pleasure steers us." />
+        <Banner text="Life is inherently meaningless. This is not a problem, it's an opportunity." />
+        <Banner text="We create meaning through our sacrifices." />
         <Banner text="Honor is an underrated virtue." />
-        <Banner text="Nature is an answer. The question is science." />
-        <Banner text="Technology makes us human." />
+        <Banner text="Make promises and keep them." />
+        <Banner
+          text="Think."
+          stylez={{
+            backgroundColor: "var(--color-blood)",
+            color: "var(--color-white)",
+          }}
+        />
+        <Banner text="Nature doesn't care about anything that matters to us. " />
+        <Banner text="The universe is an answer. The question is science." />
+        <Banner text="Imagination makes us human. " />
         <Banner text="Everything we hold dear sprouts from our imagination." />
         <Banner text="Consciousness is controlled hallucination." />
+        <Banner text="Attention is psychedelic." />
         <Banner text="What is the difference between authenticity and insanity?" />
         <Banner text="Sanity is a matter of degree." />
-        <Banner text="Norms are societal narcotics." />
-        <Banner text="Social media drama is transfat for the mind. " />
-        <Banner text="Identify with your ignorance." />
-        <Banner text="Freedom of speech improves our ability to think." />
-        <Banner text="Cancel culture is a virus to open discussions." />
+        {/* <Banner text="Norms are societal narcotics." /> */}
+        {/* <Banner text="The only way to make sense is to be insane." /> */}
+        {/* <Banner text="Social media drama is transfat for the mind. " /> */}
+        {/* <Banner text="Freedom of speech improves our ability to think." /> */}
+        {/* <Banner text="Cancel culture is a virus to open discussions." /> */}
       </main>
     </>
   );
